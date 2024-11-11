7d45fc94ae0d727acdf8222d1bebffb4802ab7dd
heruiwoniou <heruiwoniou@qq.com>
2024-11-11 17:11:04 +0800

    ci: update the release script

--- scripts/replace_version.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sed -i "s/workspace:[\^\*]/$(cat packages/monitor/package.json | jq -r '.version')/g" packages/monitor-react/package.json
-sed -i "s/workspace:[\^\*]/$(cat packages/monitor/package.json | jq -r '.version')/g" packages/monitor-sdk/package.json
